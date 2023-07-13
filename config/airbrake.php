383f6d5258187fac93146786a77363963a6b1221
Kev <kevin.j@wirebox.co.uk>
2023-07-13 14:58:32 +0100

    update depricated php-aibrake keywords to keysBlocklist

--- config/airbrake.php
+++ config/airbrake.php
@@ -12,7 +12,7 @@ return [
     'appVersion'    => null,
     'revision'      => null, #git revision
     'rootDirectory' => null,
-    'keysBlacklist' => null, #list of keys containing sensitive information that must be filtered out
+    'keysBlocklist' => null, #list of keys containing sensitive information that must be filtered out
     'httpClient'    => null, #http client implementing GuzzleHttp\ClientInterface
 
 ];
